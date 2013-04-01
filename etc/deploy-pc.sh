50dd1d9768fb00659c5199bf50cfa0c5f99c5768
Clark Ritchie <clarkritchie@yahoo.com>
2013-04-01 13:42:37 -0700

    Putting release script under version control

---
+++ etc/deploy-pc.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# initial setup:
+# mkdir poundcake; git clone -b addrpool-2 git@github.com:clarkritchie/poundcake.git
+echo "Usage: $0 [git branch] [staging]"
+
+if [ "x$1" = "x" ]; then 
+    BRANCH='clark-dev'
+else
+	BRANCH=$1
+fi
+
+if [ "x$2" = "x" ]; then 
+    TOWERDB=/var/www/towerdb.inveneo.org
+    MAINT_FILE="$TOWERDB/poundcake/app/maintenance.txt"
+else
+	TOWERDB=/var/www/towerdb-staging.inveneo.org
+fi
+
+MAINT_FILE="$TOWERDB/poundcake/app/maintenance.txt"
+
+echo "This will deploy Tower DB using:"
+echo "Branch: $BRANCH"
+echo "Path:  $TOWERDB"
+
+while true; do
+    read -p "Do you wish to deploy the site?  [Press Y to continue] " yn
+    case $yn in
+        [Yy]* )
+        	# put the site into maintenance mode
+			sudo sh -c "echo 1 > $MAINT_FILE"
+        	cd $TOWERDB/poundcake
+			sudo chown -R critchie $TOWERDB
+			git pull origin $BRANCH
+			sudo chown -R www-data $TOWERDB
+			echo "*** REMEMBER TO DISABLE MAINTENANCE MODE ***"
+			echo "    sudo sh -c \"echo 0 > $MAINT_FILE\" "
+			exit;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer Y or y to continue deployment.";;
+    esac
+done
+
+# put the site into maintenance mode
+# sudo sh -c "echo 1 > $MAINT_FILE"
+
+#cd $TOWERDB/poundcake
+#sudo chown -R critchie $TOWERDB
+#git pull origin $BRANCH
+#sudo chown -R www-data $TOWERDB
+
+#echo "*** REMEMBER TO DISABLE MAINTENANCE MODE ***"
+#echo "    sudo sh -c \"echo 0 > $MAINT_FILE\" "
